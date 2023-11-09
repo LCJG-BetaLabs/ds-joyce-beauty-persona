ddafe035e5f91997bfb54b9ac3fae18d7603c781
cintiaching <cintiaching>
2023-11-09 08:45:34 +0000

    add model features

--- profiling_features.py
+++ profiling_features.py
@@ -405,7 +405,7 @@ def tagging(desc):
     for k, v in keywords.items():
         for keyword in v:
             if keyword in desc:
-                tags.append(k)
+                tags.append(k.upper())
     return tags
 
 keywords = {
@@ -413,9 +413,9 @@ keywords = {
     "kids": ["kids", "baby"],
     "set": ["set", "bundle"],
     "serum": ["serum"],
-    "moisturizing": ["hydrat", "moistur"],
+    "moisturizing": ["hydrat", "moistur", "moist"],
     "repairing": ["repair", "restor"],
-    "whitening/rejuvenate": ["glow", "brighten", "radian", "whiten", "rejuvenat", "shine"],
+    "whitening/rejuvenate": ["glow", "brighten", "radian", "whiten", "rejuvenat", "shine", "AGING"],
     "shampoo/conditioners": ["shampoo", "conditioners"],
     "wash": ["wash"],
     "volum": ["volum"],
@@ -423,7 +423,7 @@ keywords = {
     "cream": ["cream"],
     "eye": ["eye"],
     "oil": ["oil"],
-    "fragrance": ["perfume", "edp", "fragrance"],
+    "fragrance": ["perfume", "edp", "fragrance", "edt", "parfum"],
     "lip": ["lip"],
     "candle/diffuser": ["candle", "diffuser"],
     "mask": ["mask"],
@@ -431,9 +431,22 @@ keywords = {
     "gwp": ["gwp"],
     "gold/diamond": ["gold", "diamond"],
     "brush/pencil": ["brush", "pencil"],
-    "dental": ["tooth", "teeth"]
+    "dental": ["tooth", "teeth"],
+    "XM\GIFT\CHRISTMAS": ["xm", "gift", "christmas"],
+    "gel": ["gel"],
+    "SPRAY": ["spray", "mist"],
+    "SUNSCREEN": ["sunscreen", "spf"],
+    "BLUSH": ["blush"],
+    "FOUNDATION": ["foundation"],
+    "COUPONS": ["coupons", "cash voucher"],
+    "ANTIOXIDANT": ["antioxidant"],
 }
+
 item_attr["tags"] = item_attr["item_desc"].apply(lambda x: tagging(x.lower())if x is not None else [])
+
+# COMMAND ----------
+
+spark.createDataFrame(item_attr).write.parquet(os.path.join(base_dir, "item_attr_tagging.parquet"), mode="overwrite")
 
 # COMMAND ----------
 
