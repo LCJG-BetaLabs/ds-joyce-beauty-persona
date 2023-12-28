753e16cb3d8efc63ba1ce96364880ecf375309a3
Cintia Ching <87516929+cintiaching@users.noreply.github.com>
2023-12-28 14:17:52 +0800

    [RNDG-919] revise based on business comments (#4)
    * revise
    
    * use cluster name
    
    * sum of amt by store and by month
    
    ---------
    
    Co-authored-by: cintiaching <cintiaching>

--- profiling/new_joiner_repeat_purchase.py
+++ profiling/new_joiner_repeat_purchase.py
@@ -29,13 +29,17 @@ persona.createOrReplaceTempView("persona0")
 
 # COMMAND ----------
 
+cluster_order = ["Beautyholic", "Beauty Accessories and Devices Lover", "Value Shoppers", "Personal Care Enthusiasts"]
+
+# COMMAND ----------
+
 
 def sum_pivot_table(table, group_by_col, agg_col, show_inactive=True):
     df = table.groupBy("customer_tag", group_by_col).agg(f.sum(agg_col))
     pivot_table = (
         df.groupBy(group_by_col).pivot("customer_tag").agg(f.sum(f"sum({agg_col})"))
     )
-    display(pivot_table)
+    display(pivot_table.select(group_by_col, *cluster_order))
     return pivot_table
 
 
@@ -46,7 +50,7 @@ def count_pivot_table(table, group_by_col, agg_col, percentage=False, show_inact
         .pivot("customer_tag")
         .agg(f.sum(f"count"))
     )
-    display(pivot_table)
+    display(pivot_table.select(group_by_col, *cluster_order))
     return pivot_table
 
 
@@ -56,14 +60,18 @@ def count_pivot_table(table, group_by_col, agg_col, percentage=False, show_inact
 # MAGIC create or replace temp view persona as
 # MAGIC select 
 # MAGIC   vip_main_no,
-# MAGIC   case when persona = 0 then "cluster 1"
-# MAGIC   when persona = 1 then "cluster 2"
-# MAGIC   when persona = 2 then "cluster 3"
-# MAGIC   when persona = 3 then "cluster 4" 
-# MAGIC   when persona = 4 then "cluster 4" end as persona -- the 4 and 5 cluster are merged based on profiling result
-# MAGIC from persona0;
-# MAGIC
+# MAGIC   case when persona = 0 then "Beauty Accessories and Devices Lover"
+# MAGIC   when persona = 1 then "Beautyholic"
+# MAGIC   when persona = 2 then "Value Shoppers"
+# MAGIC   when persona = 3 then "Personal Care Enthusiasts" 
+# MAGIC   when persona = 4 then "Personal Care Enthusiasts" end as persona -- the 4 and 5 cluster are merged based on profiling result
+# MAGIC from persona0
+
+# COMMAND ----------
+
+# MAGIC %sql
 # MAGIC CREATE OR REPLACE TEMP VIEW sales_cleaned AS
+# MAGIC WITH cte1 AS (
 # MAGIC select 
 # MAGIC   *,
 # MAGIC   case when maincat_desc = "SET" then "Set"
@@ -72,23 +80,112 @@ def count_pivot_table(table, group_by_col, agg_col, percentage=False, show_inact
 # MAGIC   WHEN item_subcat_desc = "dummy" then "Unknown" 
 # MAGIC   WHEN item_subcat_desc = "Dummy" THEN "Unknown"
 # MAGIC   WHEN item_subcat_desc = "SET" then "Set" ELSE item_subcat_desc END AS item_subcat_desc_cleaned
-# MAGIC from sales;
-# MAGIC
-# MAGIC CREATE OR REPLACE TEMP VIEW sales_cleaned2 AS
-# MAGIC select 
-# MAGIC   *,
-# MAGIC   concat(maincat_desc_cleaned, " - ", item_subcat_desc_cleaned) as maincat_and_subcat 
-# MAGIC from sales_cleaned;
+# MAGIC from sales
+# MAGIC ),
+# MAGIC Cte2 AS (
+# MAGIC   select 
+# MAGIC     *,
+# MAGIC     concat(maincat_desc_cleaned, " - ", item_subcat_desc_cleaned) as maincat_and_subcat 
+# MAGIC   from cte1
+# MAGIC )
+# MAGIC SELECT * FROM Cte2
+# MAGIC WHERE prod_brand not in ("JBGOT", "JB") -- Remove GOTI & Joyce Beauty from the datamart
 
 # COMMAND ----------
 
 final_sales_table = spark.sql(
     """
-    select *, 1 as dummy, persona as customer_tag from sales_cleaned2
+    select *, 1 as dummy, persona as customer_tag from sales_cleaned
     inner join persona using (vip_main_no)
     """
 )
-final_sales_table.createOrReplaceTempView("final_sales_table")
+final_sales_table.createOrReplaceTempView("final_sales_table0")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TEMP VIEW final_sales_table AS
+# MAGIC SELECT
+# MAGIC   c.vip_main_no,
+# MAGIC   shop_code,
+# MAGIC   region_key,
+# MAGIC   vip_no,
+# MAGIC   invoice_no,
+# MAGIC   item_code,
+# MAGIC   order_date,
+# MAGIC   sold_qty,
+# MAGIC   net_amt_hkd,
+# MAGIC   net_amt,
+# MAGIC   item_list_price,
+# MAGIC   shop_brand,
+# MAGIC   prod_brand,
+# MAGIC   sale_lady_id,
+# MAGIC   cashier_id,
+# MAGIC   customer_nat,
+# MAGIC   cust_nat_cat,
+# MAGIC   customer_sex,
+# MAGIC   customer_age_group,
+# MAGIC   void_flag,
+# MAGIC   valid_tx_flag,
+# MAGIC   sales_staff_flag,
+# MAGIC   sales_main_key,
+# MAGIC   sales_type,
+# MAGIC   item_desc,
+# MAGIC   item_cat,
+# MAGIC   item_sub_cat,
+# MAGIC   brand_code,
+# MAGIC   retail_price_hk,
+# MAGIC   retail_price_tw,
+# MAGIC   item_product_line_desc,
+# MAGIC   maincat_desc,
+# MAGIC   item_subcat_desc,
+# MAGIC   shop_desc,
+# MAGIC   maincat_desc_cleaned,
+# MAGIC   item_subcat_desc_cleaned,
+# MAGIC   maincat_and_subcat,
+# MAGIC   persona,
+# MAGIC   dummy,
+# MAGIC   CASE
+# MAGIC     WHEN c.customer_tag = 'Beauty Accessories and Devices Lover' THEN 'Personal Care Enthusiasts'
+# MAGIC     ELSE c.customer_tag
+# MAGIC   END AS customer_tag
+# MAGIC FROM
+# MAGIC   final_sales_table0 c
+# MAGIC WHERE
+# MAGIC   c.vip_main_no IN (
+# MAGIC     SELECT
+# MAGIC       p.vip_main_no
+# MAGIC     FROM
+# MAGIC       final_sales_table0 p
+# MAGIC     WHERE
+# MAGIC       p.prod_brand = 'JBSLP'
+# MAGIC     GROUP BY
+# MAGIC       p.vip_main_no
+# MAGIC     HAVING
+# MAGIC       COUNT(DISTINCT p.prod_brand) = 1
+# MAGIC   )
+# MAGIC UNION
+# MAGIC SELECT
+# MAGIC   *
+# MAGIC FROM
+# MAGIC   final_sales_table0 c
+# MAGIC WHERE
+# MAGIC   c.vip_main_no NOT IN (
+# MAGIC     SELECT
+# MAGIC       p.vip_main_no
+# MAGIC     FROM
+# MAGIC       final_sales_table0 p
+# MAGIC     WHERE
+# MAGIC       p.prod_brand = 'JBSLP'
+# MAGIC     GROUP BY
+# MAGIC       p.vip_main_no
+# MAGIC     HAVING
+# MAGIC       COUNT(DISTINCT p.prod_brand) = 1
+# MAGIC   )
+
+# COMMAND ----------
+
+final_sales_table = spark.table("final_sales_table")
 
 # COMMAND ----------
 
@@ -130,6 +227,47 @@ final_sales_table.createOrReplaceTempView("final_sales_table")
 # MAGIC group by
 # MAGIC   vip_main_no,
 # MAGIC   customer_tag
+
+# COMMAND ----------
+
+df = spark.sql("""select
+  distinct vip_main_no,
+  new_joiner_flag,
+  persona as customer_tag
+from
+  new_joiner
+  inner join persona using (vip_main_no)
+""")
+
+df = df.groupBy("customer_tag", "new_joiner_flag").agg(f.countDistinct("vip_main_no").alias("count"))
+pivot_table = (
+    df.groupBy("new_joiner_flag")
+    .pivot("customer_tag")
+    .agg(f.sum(f"count"))
+)
+display(pivot_table)
+
+# COMMAND ----------
+
+df = spark.sql("""select
+  distinct vip_main_no,
+  new_joiner_flag,
+  visit,
+  persona as customer_tag
+from
+  new_joiner
+  inner join visit using (vip_main_no)
+  inner join persona using (vip_main_no)
+where persona = "Beautyholic"
+""")
+
+df = df.groupBy("customer_tag", "visit").agg(f.countDistinct("vip_main_no").alias("count"))
+pivot_table = (
+    df.groupBy("visit")
+    .pivot("customer_tag")
+    .agg(f.sum(f"count"))
+)
+display(pivot_table)
 
 # COMMAND ----------
 
